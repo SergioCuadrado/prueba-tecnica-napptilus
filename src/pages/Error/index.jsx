1025d11fc71803c6e451ec82511af66fb0c6955a
SergioCuadrado <sergiocuadrado14@gmail.com>
2023-03-15 19:23:32 +0100

    add: Page error

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,15 @@
+import { Link, useRouteError } from 'react-router-dom'
+
+import './styles.css'
+
+export const ErrorPage = () => {
+  const error = useRouteError()
+
+  return (
+    <section className='error-page'>
+        <h2>{error.status} Error</h2>
+        <p>{error.data}</p>
+        <Link to='/'>Volver a la página inicial</Link>
+    </section>
+  )
+}
