c0a1ad40775063b49469f9d1a028b2f6d0453fde
SergioCuadrado <sergiocuadrado14@gmail.com>
2023-03-15 17:26:07 +0100

    add: test page productList

---
+++ tests/pages/ProductList.test.jsx
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react'
+
+import { ProductList } from '@/pages/ProductList'
+import { FilterContext } from '@/contexts/filter'
+import { ProductsContext } from '@/contexts/products'
+
+describe('Pruebas en <ProductList />', () => {
+  const filter = {
+    search: ''
+  }
+  const setProductsFilterMock = jest.fn()
+  const setProductsMock = jest.fn()
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('Snapshot de la página', () => {
+    const { container } = render(
+    <FilterContext.Provider value={{ filter, setProductsFilter: setProductsFilterMock }}>
+        <ProductsContext.Provider value={{ products: [], setProducts: setProductsMock }}>
+            <ProductList />
+        </ProductsContext.Provider>
+    </FilterContext.Provider>
+
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  test('Cuando inicias tiene que salir el spinner cargando', () => {
+    const { container } = render(
+      <FilterContext.Provider value={{ filter, setProductsFilter: setProductsFilterMock }}>
+        <ProductsContext.Provider value={{ products: [], setProducts: setProductsMock }}>
+          <ProductList />
+        </ProductsContext.Provider>
+      </FilterContext.Provider>
+    )
+
+    expect(container.querySelector('.loader')).toBeInTheDocument()
+  })
+})
