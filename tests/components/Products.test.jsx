46fa2c543d1f3612062246f291a3589934f20432
SergioCuadrado <sergiocuadrado14@gmail.com>
2023-03-16 12:46:12 +0100

    test: products, errorpage

--- tests/components/Products.test.jsx
+++ tests/components/Products.test.jsx
@@ -1,31 +1,46 @@
 import { Products } from '@/components/Products'
 import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
 
 describe('Pruebas en el componente <Products />', () => {
+  const hasProducts = [
+    {
+      id: 'YXchmkgzbCvPPUFnViAbT',
+      brand: 'Acer',
+      model: 'Liquid mini E310',
+      price: '120',
+      image: 'https://itx-frontend-test.onrender.com/images/YXchmkgzbCvPPUFnViAbT.jpg'
+    },
+    {
+      id: 'Tpt8s3EHzIPa_XxEvQJvt',
+      brand: 'Acer',
+      model: 'beTouch E210',
+      price: '150',
+      image: 'https://itx-frontend-test.onrender.com/images/Tpt8s3EHzIPa_XxEvQJvt.jpg'
+    }
+  ]
   test('Si tengo productos tengo que mostrar una lista', () => {
-    const products = [
-      {
-        id: 'YXchmkgzbCvPPUFnViAbT',
-        brand: 'Acer',
-        model: 'Liquid mini E310',
-        price: '120',
-        image: 'https://itx-frontend-test.onrender.com/images/YXchmkgzbCvPPUFnViAbT.jpg'
-      },
-      {
-        id: 'Tpt8s3EHzIPa_XxEvQJvt',
-        brand: 'Acer',
-        model: 'beTouch E210',
-        price: '150',
-        image: 'https://itx-frontend-test.onrender.com/images/Tpt8s3EHzIPa_XxEvQJvt.jpg'
-      }
-    ]
-    render(<Products products={products} />)
-    expect(screen.getAllByRole('img').length).toBe(products.length)
+    render(
+      <MemoryRouter>
+        <Products products={hasProducts} />
+      </MemoryRouter>
+    )
+    expect(screen.getAllByRole('img').length).toBe(hasProducts.length)
   })
 
   test('Si no tengo productos tengo que mostrar un mensaje', () => {
     const products = []
     render(<Products products={products} />)
     expect(screen.getByText('No hay resultados...')).toBeInTheDocument()
+  })
+
+  test('Si click en un producto tengo que ir a la pagina del producto', () => {
+    render(
+      <MemoryRouter>
+        <Products products={hasProducts} />
+      </MemoryRouter>
+    )
+    const firstProduct = screen.getAllByRole('link')[0]
+    expect(firstProduct).toHaveAttribute('href', '/products/YXchmkgzbCvPPUFnViAbT')
   })
 })
---
+++ tests/pages/Error.test.jsx
@@ -0,0 +1,37 @@
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { ErrorPage } from '@/pages/Error'
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useRouteError: () => ({
+    status: 404,
+    data: 'Not Found'
+  })
+}))
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+describe('Pruebas en la pagina <Error />', () => {
+  test('Debe de mostrarse correctamente', () => {
+    const { container } = render(
+        <MemoryRouter initialEntries={['/prueba']}>
+            <ErrorPage />
+        </MemoryRouter>
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  test('Si click en el boton de volver a la pagina inicial, debe de redirigir a la pagina inicial', () => {
+    const { getByText } = render(
+        <MemoryRouter initialEntries={['/prueba']}>
+            <ErrorPage />
+        </MemoryRouter>
+    )
+    const link = getByText('Volver a la página inicial')
+    expect(link.getAttribute('href')).toBe('/')
+  })
+})
