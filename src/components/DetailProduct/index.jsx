678e6d03b1da86d3b9b8f5b3bc5dc0efee08a1ab
SergioCuadrado <sergiocuadrado14@gmail.com>
2023-03-17 13:23:59 +0100

    fix: select default value

--- src/components/DetailProduct/index.jsx
+++ src/components/DetailProduct/index.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import Select from 'react-select'
 import { useAddProduct } from '../../hooks/useAddProduct'
-
 import './styles.css'
 
 export const DetailProduct = ({ product, descriptionProducts, optionsProduct }) => {
-  const storageDefault = optionsProduct.storages.length === 1 ? optionsProduct.storages[0].name : ''
+  const storageDefault = optionsProduct.storages.length === 1
+    ? {
+        value: optionsProduct.storages[0].code,
+        label: optionsProduct.storages[0].name
+      }
+    : ''
   const colorDefault = optionsProduct.colors.length === 1 ? optionsProduct.colors[0].code : ''
-
-  const [actions, setActions] = useState({ colorCode: colorDefault, storageCode: storageDefault })
+  const StorageOptions = optionsProduct.storages.map((color) => ({ value: color.code, label: color.name }))
+  const [actions, setActions] = useState({ colorCode: colorDefault, storageCode: storageDefault?.value ? storageDefault.value : '' })
   const { getAddProductCart } = useAddProduct()
   if (!optionsProduct) return null
 
@@ -19,11 +24,10 @@ export const DetailProduct = ({ product, descriptionProducts, optionsProduct }) 
     })
   }
 
-  const handleStorageChange = ({ target }) => {
-    const storageCode = target.value
+  const handleStorageChange = ({ value }) => {
     setActions({
       ...actions,
-      storageCode
+      storageCode: value
     })
   }
 
@@ -44,47 +48,64 @@ export const DetailProduct = ({ product, descriptionProducts, optionsProduct }) 
       <div className="details">
           <div className="content">
             <h2>{product.description.model}</h2>
-            <table>
-                <tbody>
-                  {descriptionProducts
-                    ? descriptionProducts.map((description) => (
-                    <tr key={description.name}>
-                      <td>
-                        <span>
-                          {description.name}
-                        </span>
-                      </td>
-                      <td>
-                        <span>
-                          {description.value.length > 0 ? description.value : 'No especificado'}
-                        </span>
-                      </td>
-                    </tr>
-                    ))
-                    : null}
-                </tbody>
-            </table>
-            <form className='buy-product' onSubmit={handleSubmit}>
-                <label className='capacity' htmlFor="capacity">Capacidad</label>
-                <select name="capacity" id="capacity" onChange={handleStorageChange}>
-                    {optionsProduct.storages.map((capacity) => (
-                        <option key={capacity.code} value={capacity.name}>{capacity.name}</option>
-                    ))}
-                </select>
-                <h4 className='colors-title'>Colores disponibles:</h4>
-                <div className="product-colors" >
-                {optionsProduct.colors.map((color) => (
-                    <div key={color.code}>
-                        <label className={`${color.name} ${actions.colorCode === color.code ? 'active' : null}`} style={{ backgroundColor: color?.name.toLowerCase() }} htmlFor={color.code} />
-                        <input type="checkbox" name={color.code} id={color.code} onChange={handleColorsChange} checked={actions.color === color.code} hidden />
-                    </div>
-                ))}
-                </div>
-                <h3>$ {product.description.price}</h3>
-                <button disabled={actions.colorCode === '' || actions.storageCode === ''}>Comprar</button>
-            </form>
+            <TableDescription descriptionProducts={descriptionProducts} />
+            <Form handleSubmit={handleSubmit} optionsProduct={optionsProduct} actions={actions} handleColorsChange={handleColorsChange} product={product} storageDefault={storageDefault} handleStorageChange={handleStorageChange} StorageOptions={StorageOptions} />
           </div>
       </div>
     </div>
+  )
+}
+
+const TableDescription = ({ descriptionProducts }) => (
+  <table>
+      <tbody>
+        {descriptionProducts
+          ? descriptionProducts.map((description) => (
+          <tr key={description.name}>
+            <td>
+              <span>
+                {description.name}
+              </span>
+            </td>
+            <td>
+              <span>
+                {description.value.length > 0 ? description.value : 'No especificado'}
+              </span>
+            </td>
+          </tr>
+          ))
+          : null}
+      </tbody>
+  </table>
+)
+
+const Form = ({ handleSubmit, optionsProduct, actions, handleColorsChange, product, storageDefault, handleStorageChange, StorageOptions }) => {
+  const colourStyles = {
+    control: (styles) => ({ ...styles, backgroundColor: 'white' }),
+    option: (styles) => {
+      return {
+        ...styles,
+        backgroundColor: 'white',
+        color: 'black'
+      }
+    }
+  }
+
+  return (
+    <form className='buy-product' onSubmit={handleSubmit}>
+        <h4 className='capacity'>Capacidad</h4>
+        <Select className='basic-single' aria-label='Capacidad' onChange={handleStorageChange} options={StorageOptions} defaultInputValue={storageDefault?.label} styles={colourStyles} placeholder="Selecciona la capacidad de almacenamiento que desea" />
+        <h4 className='colors-title'>Colores disponibles:</h4>
+        <div className="product-colors" >
+        {optionsProduct.colors.map((color) => (
+            <div key={color.code}>
+                <label className={`${color.name} ${actions.colorCode === color.code ? 'active' : null}`} style={{ backgroundColor: color?.name.toLowerCase() }} htmlFor={color.code} />
+                <input type="checkbox" name={color.code} id={color.code} onChange={handleColorsChange} checked={actions.color === color.code} hidden />
+            </div>
+        ))}
+        </div>
+        <h3>$ {product.description.price}</h3>
+        <button disabled={actions.colorCode === '' || actions.storageCode === ''}>Comprar</button>
+    </form>
   )
 }
